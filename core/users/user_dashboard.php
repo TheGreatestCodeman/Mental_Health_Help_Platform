b9f721418e0f3c41ffc4326e5d883d94815fbdea
Tanvir Rahman Tonmoy <133966200+TRTonmoy1@users.noreply.github.com>
2026-01-05 20:44:34 +0600

    Update user_dashboard.php

--- core/users/user_dashboard.php
+++ core/users/user_dashboard.php
@@ -1,6 +1,6 @@
 <?php
-require_once(__DIR__ . "/../../config/database.php");
-//require_once('database.php');
+//require_once(__DIR__ . "/../../config/database.php");
+require_once('database.php');
 session_start();
 
 $user_id = $_SESSION['user_id']??8;
@@ -96,4 +96,5 @@ $pending = mysqli_fetch_assoc($pendingResult)['total'];
 
 </body>
 </html>
+
 
