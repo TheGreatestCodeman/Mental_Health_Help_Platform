33092b3f24cc3dfb9ec952e7c55717d82fa06a94
Tanvir Rahman Tonmoy <133966200+TRTonmoy1@users.noreply.github.com>
2026-01-05 20:40:53 +0600

    Update appointment_book.php

--- core/users/appointment_book.php
+++ core/users/appointment_book.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-require_once(__DIR__ . "/../../config/database.php");
-//require_once("database.php");
+//require_once(__DIR__ . "/../../config/database.php");
+require_once("database.php");
 //if (!isset($_SESSION['user_id'])) {
    // header("Location: ../../login.php");
   //  exit;
@@ -36,4 +36,5 @@ $stmt->execute();
 
 header("Location: my_appointments.php");
 exit;
+
 
