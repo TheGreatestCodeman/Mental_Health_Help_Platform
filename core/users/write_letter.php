667a3968aa5b4467724b5c86c8f0889adf308bec
Tanvir Rahman Tonmoy <133966200+TRTonmoy1@users.noreply.github.com>
2026-01-05 19:14:04 +0600

    Update write_letter.php

--- core/users/write_letter.php
+++ core/users/write_letter.php
@@ -1,5 +1,6 @@
 <?php
-include "database.php";
+//include "database.php";
+require_once(__DIR__ . "/../../config/database.php");
 session_start();
 
 
@@ -84,5 +85,6 @@ if (isset($_POST['save_letter'])) {
 
 </body>
 </html>
+
 
 
